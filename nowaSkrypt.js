74735725e62dc36489e67008a4ad4cf60313d15f
Adikko <adriankulik93@gmail.com>
2020-06-18 17:59:24 +0200

    script divided in order to simplify copy and pasting

--- nowaSkrypt.js
+++ nowaSkrypt.js
@@ -189,25 +189,3 @@ for (let i = 0; i < finalArticleArrayWithPhotos.length; i++) // wpisuje edytowan
 }
 
 document.getElementsByTagName("iframe")[0].contentWindow.document.getElementById("tinymce").innerHTML = editedArticle;
-
-
-
-
-// ___________STARA
-
-// AUTOR:
-console.log('<p style="text-align: right;">' + document.getElementsByClassName("authors")[0].innerText + '</p>')
-
-// CYTAT:
-for (let i = 0; i < document.getElementsByTagName("blockquote").length; i++)
-{
-    console.log("<blockquote>\n" + document.getElementsByTagName("blockquote")[i].innerText + "</blockquote>");
-}
-
-// DŁUGOŚĆ ARTYKUŁÓW
-// Stara: (uwzglednia niestety date)
-document.getElementsByClassName("medium-8 columns content")[0].innerText.length;
-// Nowa:
-let letterCount1 = document.getElementsByTagName("iframe")[0].contentWindow.document.getElementById("tinymce").innerText.length;
-let letterCount2 = document.getElementById("description").value.length;
-letterCount1 + letterCount2;
