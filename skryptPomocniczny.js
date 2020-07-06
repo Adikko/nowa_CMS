b0cb2e62f5e69235c96e10d571df5a81d828da30
Adikko <adriankulik93@gmail.com>
2020-07-06 21:01:21 +0200

    diagnostic script designet to work on the old site

--- skryptPomocniczny.js
+++ skryptPomocniczny.js
@@ -1,12 +1,25 @@
-// AUTOR:
-console.log('<p style="text-align: right;">' + document.getElementsByClassName("authors")[0].innerText + '</p>')
+// Skrypt do aktywacji w aktualnościach na starej stronie UPWr
 
-// CYTAT:
+// DŁUGOSĆ ARTYKUŁÓW: (uwzglednia niestety date)
+console.log("długość artykułu: " + document.getElementsByClassName("medium-8 columns content")[0].innerText.length + " znaków.");
+
+// AUTOR:
+console.log('autor artykułu: ' + document.getElementsByClassName("authors")[0].innerText)
+
+// CYTATY:
+let quoteArray = [];
 for (let i = 0; i < document.getElementsByTagName("blockquote").length; i++)
 {
-    console.log("<blockquote>\n" + document.getElementsByTagName("blockquote")[i].innerText + "</blockquote>");
+    quoteArray.push(document.getElementsByTagName("blockquote")[i].innerText);
 }
+console.log("Ilość cytatów: " + quoteArray.length);
+console.log(quoteArray);
 
-// DŁUGOŚĆ ARTYKUŁÓW
-// Stara: (uwzglednia niestety date)
-document.getElementsByClassName("medium-8 columns content")[0].innerText.length;
+// LINKI:
+let allLinks = document.getElementsByClassName("medium-8 columns content")[0].getElementsByTagName("a").length; //zliczam wszystkie linki (niestety łącznie z tagami)
+let tagsLinks = document.getElementsByClassName("tags")[0].getElementsByTagName("a").length; //zliczam linki
+let articleLinks = allLinks - tagsLinks;
+console.log("Ilość linków: " + articleLinks);
+
+// ZDJĘCIA:
+console.log("Ilość zdjęć: " + document.getElementsByClassName("medium-8 columns content")[0].getElementsByTagName("img").length);
