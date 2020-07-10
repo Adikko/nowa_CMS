4ee9d78c113b52a708e96bd85af4e87947c18174
Adikko <adriankulik93@gmail.com>
2020-07-10 12:11:24 +0200

    fixed script not seeing the existance of gallery

--- skryptPomocniczny.js
+++ skryptPomocniczny.js
@@ -1,5 +1,12 @@
 // Skrypt do aktywacji w aktualnościach na starej stronie UPWr
 
+// Sprawdzam jakie są węzły artykułu i zapisuję je w nodeArray
+let nodeArray = [];
+for (let i = 0; i < document.getElementById("content").children.length; i++)
+{
+    nodeArray.push(document.getElementById("content").children[i].className);
+}
+
 // DŁUGOSĆ ARTYKUŁÓW: (uwzglednia niestety date)
 console.log("Długość artykułu: " + document.getElementsByClassName("medium-8 columns content")[0].innerText.length + " znaków. Ta liczba może się różnić od liczby z panelu Nowej o +/- 10%.");
 
@@ -17,10 +24,9 @@ if (quoteArray.length > 0)
 
 // LINKI:
 let imageLinks;
-
 let allLinks = document.getElementsByClassName("medium-8 columns content")[0].getElementsByTagName("a").length; //zliczam wszystkie linki (niestety łącznie z tagami)
 let tagsLinks = document.getElementsByClassName("tags")[0].getElementsByTagName("a").length; //zliczam linki
-if (document.getElementsByClassName("row body")[0].classList.contains("images naked floatbox")) //sprawdzam, czy strona ma galerię. Nie mogę liczyć nieistniejącego elementu...
+if (nodeArray.includes("images naked floatbox")) //sprawdzam, czy strona ma galerię. Nie mogę liczyć nieistniejącego elementu...
 {
     imageLinks = document.getElementsByClassName("images naked floatbox")[0].getElementsByTagName("a").length; //każdy obrazek podglądowy jest linkiem do zdjęcia w pełnej rodzielczosci, trzeba to wziac pod uwage
 }
@@ -34,7 +40,19 @@ let articleLinks = allLinks - tagsLinks - socialLinks - imageLinks;
 console.log("Ilość linków: " + articleLinks);
 
 // ZDJĘCIA:
-console.log("Ilość zdjęć: " + document.getElementsByClassName("medium-8 columns content")[0].getElementsByTagName("img").length);
+let everyImage = document.getElementsByClassName("medium-8 columns content")[0].getElementsByTagName("img").length;
+let galleryImages;
+if (nodeArray.includes("images naked floatbox")) //sprawdzam, czy strona ma galerię. Nie mogę liczyć nieistniejącego elementu...
+{
+    galleryImages = document.getElementsByClassName("images naked floatbox")[0].getElementsByTagName("img").length;
+    console.log("Jest galeria.");
+}
+else
+{
+    galleryImages = 0;
+}
+let imageCount = everyImage - galleryImages;
+console.log("Ilość zdjęć: " + imageCount);
 
 
 // AUTOR:
